dfceb7e3585e99e31f2c75c40e3afa45e1d97a37
jyo1706 <93206041+jyo1706@users.noreply.github.com>
2023-06-28 20:23:27 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose')
 url='mongodb://127.0.0.1:27017/pra'
-live_Url="mongodb+srv://jyotiuser:jyoti1706@cluster0.j4lrbou.mongodb.net/?retryWrites=true&w=majority"
+live_Url="mongodb+srv://jyotiuser:jyoti1706@cluster0.j4lrbou.mongodb.net/pra?retryWrites=true&w=majority"
 const connectDb = ()=>
 {
     return mongoose.connect(live_Url)
